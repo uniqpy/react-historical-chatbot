da929fc59c104834e311cd8f2c01b806bd75cb9d
Kablooy <nathanbushell@outlook.com>
2025-12-09 18:32:32 +0000

    added help modal and minor ui changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Routes, Route, Link, Outlet } from 'react-router-dom';
 import ChatFullPage from './pages/ChatFullPage.jsx';
 import AboutDocs from './pages/AboutDocs.jsx';
-import HelpPage from './pages/HelpPage.jsx';
+import { Modal } from "bootstrap";
+import { useRef, useEffect } from "react";
+
+
 
 /**
  * This function shows the links to the other pages on the top of the page. 
@@ -9,12 +12,57 @@ import HelpPage from './pages/HelpPage.jsx';
  * @returns
  */
 function TopLinks() {
+  const modalRef = useRef(null);
+  const bsModal = useRef(null);
+
+  useEffect(() => {
+    bsModal.current = new Modal(modalRef.current, {
+      backdrop: "static",
+      keyboard: false,
+    });
+  }, []);
+
+  const openModal = () => {
+    bsModal.current.show();
+  };
+
+  const closeModal = () => {
+    bsModal.current.hide();
+  };
+
+
+
   return (
     <div className="container py-2">
       <div className="bg-white p-3 rounded shadow-sm d-inline-flex align-items-center gap-3">
-        <Link to="/" className="text-decoration-none fw-semibold">Historical Chatbot</Link>
+        <Link to="/" className="text-decoration-none fw-semibold">Caligula Chatbot</Link>
         <Link to="/about" className="text-decoration-none text-secondary">About</Link>
-        <Link to="/help" className="text-decoration-none text-secondary">Help</Link>
+        <button className="text-decoration-none text-secondary" onClick={openModal}>Help</button>
+         <div
+        className="modal fade"
+        tabIndex="-1"
+        ref={modalRef}
+      >
+        <div className = "modal-dialog">
+          <div className = "modal-content">
+            <div className = "modal-header">
+              <h5 className = "modal-title">Help Pop-up</h5>
+            </div>
+
+            <div className="modal-body">
+              <h3>Need help?</h3>
+              <p>If you need help, contact us at CaligulaNeedsHelp@caligula.net</p>
+            </div>
+            <div className="modal-footer">
+              <button className="btn btn-secondary" onClick={closeModal}>Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+       
       </div>
     </div>
   );
@@ -42,7 +90,6 @@ export default function App() {
       <Route element={<Layout />}>
         <Route index element={<ChatFullPage />} />
         <Route path="about" element={<AboutDocs />} />
-        <Route path="help" element={<HelpPage/>} />
       </Route>
     </Routes>
   );
